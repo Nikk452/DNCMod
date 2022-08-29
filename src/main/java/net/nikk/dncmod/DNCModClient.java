8d412cb9071cfdb1a950a7d0638c76fc445d8173
Nikk <Nikk2002os@gmail.com>
2022-08-29 20:05:12 +0300

    Initial commit

---
+++ src/main/java/net/nikk/dncmod/DNCModClient.java
@@ -0,0 +1,10 @@
+package net.nikk.dncmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DNCModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
